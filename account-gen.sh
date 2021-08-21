3e45b0ed6939e9e5acd036006c1eef67ca158aba
Kami1983 <meetcancanyou@yahoo.com>
2021-08-21 18:03:03 +0800

    Upgrade Substrate to the monthly-2021-08 version and optimize the framework, update the pallet-ares and pallet-owc components.

--- account-gen.sh
+++
@@ -1,9 +0,0 @@
-
-echo -n "Input SECRET"
-read SECRET
-echo -n "Input key: $SECRET"
-
-for i in 1 2 3 4; do for j in stash controller; do subkey inspect "$SECRET//$i//$j"; done; done
-for i in 1 2 3 4; do for j in babe; do subkey inspect "$SECRET//$i//$j" --scheme Sr25519; done; done
-for i in 1 2 3 4; do for j in grandpa; do subkey inspect "$SECRET//$i//$j" --scheme Ed25519; done; done
-for i in 1 2 3 4; do for j in im_online; do subkey inspect "$SECRET//$i//$j" --scheme Sr25519; done; done
